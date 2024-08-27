787ee2ea8349976f1308fd36408d7957c15b271f
André Ruan <andreruan80@gmail.com>
2024-08-27 02:32:09 -0300

    Remover usings desnecessários

--- GeradorDePacotes/FormsUC/Frm_ConfigUC.cs
+++ GeradorDePacotes/FormsUC/Frm_ConfigUC.cs
@@ -1,14 +1,4 @@
-﻿using GeradorDePacotes.FormsUC;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
+﻿
 namespace GeradorDePacotes
 {
     public partial class Frm_ConfigUC : UserControl
