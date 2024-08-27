a98cb030321fafef82534aa415a75a43b56830f7
André Ruan <andreruan80@gmail.com>
2024-08-27 02:31:41 -0300

    Adiciona classe `Helpers` para agrupar métodos auxiliares como o de centralizar o conteúdo dos painéis UserControl

---
+++ GeradorDePacotes/Classes/Helpers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeradorDePacotes.Classes
+{
+    public static class Helpers
+    {
+        public static void CenterPanel(Control frmParent, Control sideBar, Control ctrlContent, bool expandedBar)
+        {
+            int x;
+            int y = (frmParent.Height - ctrlContent.Height) / 2;
+
+            if (expandedBar)
+            {
+                x = (frmParent.Width - sideBar.Width - ctrlContent.Width) / 2 + sideBar.Width;
+            }
+            else
+            {
+                x = (frmParent.Width - sideBar.Width - ctrlContent.Width) / 2 + sideBar.Width;
+            }
+
+            ctrlContent.Location = new Point(x, y);
+        }
+
+    }
+}
