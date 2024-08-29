cb463172666d4545f28055c51d2a58ae4d817420
André Ruan <andreruan80@gmail.com>
2024-08-29 00:53:43 -0300

    Feat: Adicionado métodos para consultar/atualizar/inserir parâmetros

---
+++ GeradorDePacotes/Database/Utils.cs
@@ -0,0 +1,41 @@
+﻿using GeradorDePacotes.Database.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeradorDePacotes.Database
+{
+    public class Utils
+    {
+        public static void AddOrUpdateParKeys(ApplicationDbContext ctx, string parName, string parValue)
+        {
+            var par = ctx.ParKeys
+                .Where(x => x.ParName.Equals(parName))
+                .FirstOrDefault();
+
+            if (par != null)
+            {
+                par.ParValue = parValue;
+                ctx.SaveChanges();
+            }
+            else
+            {
+                var newParKey = new ParKey
+                {
+                    ParName = parName,
+                    ParValue = parValue
+                };
+
+                ctx.ParKeys.Add(newParKey);
+                ctx.SaveChanges();
+            }
+        }
+
+        public static string GetParValue(ApplicationDbContext ctx, string parName)
+        {
+            return ctx.ParKeys.Where(x=>x.ParName.Equals(parName)).Select(x=>x.ParValue).FirstOrDefault();
+        }
+    }
+}
