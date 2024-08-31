0a66d31e51f251f8ca39aa894c37cb8bcee5a3ee
André Ruan <andreruan80@gmail.com>
2024-08-31 03:00:17 -0300

    feat: Adicionado método para extrair o nome do arquivo/pasta sem o diretório

--- GeradorDePacotes/Classes/Helpers.cs
+++ GeradorDePacotes/Classes/Helpers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,26 +9,26 @@ namespace GeradorDePacotes.Classes
 {
     public static class Helpers
     {
-        public static void CenterPanelSideBar(Control frmParent, Control sideBar, Control ctrlContent, bool expandedBar)
+        public static void CenterPanelSideBar(Control frmParent, Control sideBar, Control panelTop, Control ctrlContent, bool expandedBar)
         {
             int x;
-            int y = (frmParent.Height - ctrlContent.Height) / 2;
+            int y = (frmParent.ClientSize.Height - panelTop.ClientSize.Height - ctrlContent.ClientSize.Height) / 2 + panelTop.Height;
 
             if (expandedBar)
             {
-                x = (frmParent.Width - sideBar.Width - ctrlContent.Width) / 2 + sideBar.Width;
+                x = (frmParent.ClientSize.Width - sideBar.ClientSize.Width - ctrlContent.ClientSize.Width) / 2 + sideBar.ClientSize.Width;
             }
             else
             {
-                x = (frmParent.Width - sideBar.Width - ctrlContent.Width) / 2 + sideBar.Width;
+                x = (frmParent.ClientSize.Width - sideBar.ClientSize.Width - ctrlContent.ClientSize.Width) / 2 + sideBar.ClientSize.Width;
             }
 
             ctrlContent.Location = new Point(x, y);
         }
         public static void CenterPanel(Control ctrlParent, Control ctrlChild)
         {
-            int x = (ctrlParent.Width - ctrlChild.Width) / 2;
-            int y = (ctrlParent.Height - ctrlChild.Height) / 2;
+            int x = (ctrlParent.ClientSize.Width - ctrlChild.ClientSize.Width) / 2;
+            int y = (ctrlParent.ClientSize.Height - ctrlChild.ClientSize.Height) / 2;
             ctrlChild.Location = new Point(x, y);
         }
 
@@ -65,5 +66,36 @@ namespace GeradorDePacotes.Classes
             return true;
         }
 
+        public static string GetNameWithoutPath(string path)
+        {
+            char charSeparator = Path.DirectorySeparatorChar;
+            var lastIndex = path.LastIndexOf(charSeparator);
+            var nameFolderWithoutPath = path.Substring(lastIndex + 1);
+            return nameFolderWithoutPath;
+        }
+
+        public static string GetNameWithoutPathForTargetFolder(string path, string pathTargetFolder)
+        {
+            if (path.Equals(pathTargetFolder))
+            {
+                char charSeparatorTarget = Path.DirectorySeparatorChar;
+                var index = pathTargetFolder.LastIndexOf(charSeparatorTarget);
+                return pathTargetFolder.Substring(index + 1);
+            }
+            char charSeparator = Path.DirectorySeparatorChar;
+            var lastIndexCharSeparator = pathTargetFolder.LastIndexOf(charSeparator);
+            var nameTargetFolderWithoutPath = pathTargetFolder.Substring(lastIndexCharSeparator + 1);
+            var lastIndex = path.IndexOf(nameTargetFolderWithoutPath);
+            var pathAfterTargetFolder = path.Substring(lastIndex + nameTargetFolderWithoutPath.Length + 1);
+            return pathAfterTargetFolder;
+        }
+
+        public static IEnumerable<T> GetAllControlsOfType<T>(Control parent) where T : Control
+        {
+            var controls = parent.Controls.OfType<T>();
+
+            return controls.Concat(parent.Controls.OfType<Control>()
+                                        .SelectMany(ctrl => GetAllControlsOfType<T>(ctrl)));
+        }
     }
 }
