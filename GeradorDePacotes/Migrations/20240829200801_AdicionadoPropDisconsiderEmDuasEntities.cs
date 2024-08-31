c02c0ec2dc3b2a3b82c27499ce55ebf65956b529
André Ruan <andreruan80@gmail.com>
2024-08-31 03:06:35 -0300

    feat: Migration necessária para incluir duas propriedades em duas entities

---
+++ GeradorDePacotes/Migrations/20240829200801_AdicionadoPropDisconsiderEmDuasEntities.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeradorDePacotes.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionadoPropDisconsiderEmDuasEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Disconsider",
+                table: "FoldersToVerify",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Disconsider",
+                table: "FilesToVerify",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Disconsider",
+                table: "FoldersToVerify");
+
+            migrationBuilder.DropColumn(
+                name: "Disconsider",
+                table: "FilesToVerify");
+        }
+    }
+}
