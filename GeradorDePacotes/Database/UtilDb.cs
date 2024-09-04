bbb2f74c8e2d6f8e721fd3c27e3f0f718bb454ca
André Ruan <andreruan80@gmail.com>
2024-09-03 23:37:25 -0300

    Feat: Alteração dos métodos para Async

--- GeradorDePacotes/Database/UtilDb.cs
+++ GeradorDePacotes/Database/UtilDb.cs
@@ -1,12 +1,6 @@
 ﻿using GeradorDePacotes.Classes;
 using GeradorDePacotes.Database.Entities;
 using Microsoft.EntityFrameworkCore;
-using SQLitePCL;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GeradorDePacotes.Database
 {
@@ -15,19 +9,17 @@ namespace GeradorDePacotes.Database
     {
         #region["DataGrids"]
 
-        public static void AddOrUpdateToGridFoldersToDelete(ApplicationDbContext ctx, string pathFolder, string pathTargetFolder, bool disconsider = false)
+        public async static Task AddOrUpdateToGridFoldersToDeleteAsync(ApplicationDbContext ctx, string pathFolder, string pathTargetFolder, bool disconsider = false)
         {
             var nameFolder = Helpers.GetNameWithoutPathForTargetFolder(pathFolder, Frm_ConfigUC.PathTargetFolder);
 
-            var folderInDb = ctx.FoldersToDelete
-                .AsEnumerable()
-                .FirstOrDefault(f => f.NameFolder.Equals(nameFolder, StringComparison.OrdinalIgnoreCase));
+            var folderInDb = await ctx.FoldersToDelete
+                .AsQueryable()
+                .FirstOrDefaultAsync(f => f.NameFolder.ToLower() == nameFolder.ToLower());
 
             if (folderInDb != null)
             {
                 folderInDb.NameFolder = nameFolder;
-                ctx.SaveChanges();
-
             }
             else
             {
@@ -38,23 +30,22 @@ namespace GeradorDePacotes.Database
                 };
 
                 ctx.Add(folder);
-                ctx.SaveChanges();
             }
+            await ctx.SaveChangesAsync();
         }
 
-        public static void AddOrUpdateToGridFilesToDelete(ApplicationDbContext ctx, string[] paths, bool disconsider = false)
+        public async static Task AddOrUpdateToGridFilesToDeleteAsync(ApplicationDbContext ctx, string[] paths, bool disconsider = false)
         {
             foreach (var path in paths)
             {
-               var nameFile =  Helpers.GetNameWithoutPathForTargetFolder(path, Frm_ConfigUC.PathTargetFolder);
-                var fileInDb = ctx.FilesToDelete
-                    .AsEnumerable()
-                    .FirstOrDefault(f => f.NameFile.Equals(nameFile, StringComparison.OrdinalIgnoreCase));
+                var nameFile = Helpers.GetNameWithoutPathForTargetFolder(path, Frm_ConfigUC.PathTargetFolder);
+                var fileInDb = await ctx.FilesToDelete
+                    .AsQueryable()
+                    .FirstOrDefaultAsync(f => f.NameFile.ToLower() == nameFile.ToLower());
 
                 if (fileInDb != null)
                 {
                     fileInDb.NameFile = nameFile;
-                    ctx.SaveChanges();
                 }
                 else
                 {
@@ -65,23 +56,23 @@ namespace GeradorDePacotes.Database
                     };
 
                     ctx.Add(folder);
-                    ctx.SaveChanges();
                 }
+                await ctx.SaveChangesAsync();
             }
         }
 
-        public static void AddOrUpdateToGridFoldersToVerify(ApplicationDbContext ctx, string pathFolder, bool disconsider = false)
-        {
-            var nameFolder = Helpers.GetNameWithoutPath(pathFolder);
 
-            var folderInDb = ctx.FoldersToVerify
-                .AsEnumerable()
-                .FirstOrDefault(f => f.NameFolder.Equals(nameFolder, StringComparison.OrdinalIgnoreCase));
+        public async static Task AddOrUpdateToGridFoldersToVerifyAsync(ApplicationDbContext ctx, string pathFolder, bool disconsider = false)
+        {
+            var nameFolder = Helpers.GetNameWithoutPathForTargetFolder(pathFolder, Frm_ConfigUC.PathTargetFolder);
+
+            var folderInDb = await ctx.FoldersToVerify
+                .AsQueryable()
+                .FirstOrDefaultAsync(f => f.NameFolder.ToLower() == nameFolder.ToLower());
 
             if (folderInDb != null)
             {
                 folderInDb.NameFolder = nameFolder;
-                ctx.SaveChanges();
             }
             else
             {
@@ -92,34 +83,34 @@ namespace GeradorDePacotes.Database
                 };
 
                 ctx.Add(folder);
-                ctx.SaveChanges();
             }
+            await ctx.SaveChangesAsync();
         }
 
-        public static void AddOrUpdateToGridFilesToVerify(ApplicationDbContext ctx, string[] names, bool disconsider = false)
+        public async static Task AddOrUpdateToGridFilesToVerifyAsync(ApplicationDbContext ctx, string[] names, bool disconsider = false)
         {
             foreach (var name in names)
             {
-                var fileInDb = ctx.FilesToDelete
-                    .AsEnumerable()
-                    .FirstOrDefault(f => f.NameFile.Equals(name, StringComparison.OrdinalIgnoreCase));
+                var pathFile = Helpers.GetNameWithoutPathForTargetFolder(name, Frm_ConfigUC.PathTargetFolder);
+                var fileInDb = await ctx.FilesToVerify
+                    .AsQueryable()
+                    .FirstOrDefaultAsync(f => f.NameFile.ToLower() == pathFile.ToLower());
 
                 if (fileInDb != null)
                 {
-                    fileInDb.NameFile = name;
-                    ctx.SaveChanges();
+                    fileInDb.NameFile = pathFile;
                 }
                 else
                 {
-                    var folder = new FilesToDelete
+                    var file = new FilesToVerify
                     {
-                        NameFile = name,
+                        NameFile = pathFile,
                         Disconsider = disconsider
                     };
 
-                    ctx.Add(folder);
-                    ctx.SaveChanges();
+                    ctx.Add(file);
                 }
+                await ctx.SaveChangesAsync();
             }
         }
 
@@ -127,121 +118,113 @@ namespace GeradorDePacotes.Database
 
         #region["Tables"]
 
-        public static void AddOrUpdateTableFoldersToDelete(ApplicationDbContext ctx, string? name = null, int? id = null, bool? disconsider = null)
+        public async static Task AddOrUpdateTableFoldersToDeleteAsync(ApplicationDbContext ctx, string? name = null, int? id = null, bool? disconsider = null)
         {
             if (string.IsNullOrWhiteSpace(name) && id == null)
-                throw new Exception("O nome e o ID não podem ser nulos nesse método");
+                throw new ArgumentException("O nome e o ID não podem ser nulos nesse método");
 
-            var fileInDb = ctx.FoldersToDelete
-                .AsEnumerable()
-                .FirstOrDefault(f => (string.IsNullOrWhiteSpace(name) || f.NameFolder.Equals(name, StringComparison.OrdinalIgnoreCase))
-                                     && (id == null || f.Id == id));
+            var fileInDb = await ctx.FoldersToDelete
+                .AsQueryable()
+                .FirstOrDefaultAsync(f => id == null || f.Id == id);
 
             if (fileInDb != null)
             {
-                fileInDb.NameFolder = string.IsNullOrWhiteSpace(name) ? fileInDb.NameFolder : name ;
-                fileInDb.Disconsider = disconsider == null ? fileInDb.Disconsider : (bool)disconsider;
+                fileInDb.NameFolder = string.IsNullOrWhiteSpace(name) ? fileInDb.NameFolder : name;
+                fileInDb.Disconsider = disconsider ?? fileInDb.Disconsider;
             }
             else
             {
                 var folder = new FoldersToDelete
                 {
                     NameFolder = string.IsNullOrWhiteSpace(name) ? "Erro!" : name,
-                    Disconsider = disconsider == null ? false : (bool)disconsider
+                    Disconsider = disconsider ?? false
                 };
                 ctx.Add(folder);
             }
-                ctx.SaveChanges();
+            await ctx.SaveChangesAsync(); // SaveChangesAsync não bloqueia a UI
         }
-
-        public static void AddOrUpdateTableFilesToDelete(ApplicationDbContext ctx, string? name = null, int? id = null, bool? disconsider = null)
+       
+        public async static Task AddOrUpdateTableFilesToDeleteAsync(ApplicationDbContext ctx, string? name = null, int? id = null, bool? disconsider = null)
         {
             if (string.IsNullOrWhiteSpace(name) && id == null)
-                throw new Exception("O nome e o ID não podem ser nulos nesse método");
+                throw new ArgumentException("O nome e o ID não podem ser nulos nesse método");
 
-            var fileInDb = ctx.FilesToDelete
-                .AsEnumerable()
-                .FirstOrDefault(f => (string.IsNullOrWhiteSpace(name) || f.NameFile.Equals(name, StringComparison.OrdinalIgnoreCase))
-                                     && (id == null || f.Id == id));
+            var fileInDb = await ctx.FilesToDelete
+                .AsQueryable()
+                .FirstOrDefaultAsync(f => id == null || f.Id == id);
 
             if (fileInDb != null)
             {
                 fileInDb.NameFile = !string.IsNullOrWhiteSpace(name) ? name : fileInDb.NameFile;
-                fileInDb.Disconsider = disconsider == null ? fileInDb.Disconsider : (bool)disconsider;
-                ctx.SaveChanges();
-
+                fileInDb.Disconsider = disconsider ?? fileInDb.Disconsider;
             }
             else
             {
                 var file = new FilesToDelete
                 {
                     NameFile = !string.IsNullOrWhiteSpace(name) ? name : "Erro!",
-                    Disconsider = disconsider == null ? false : (bool)disconsider
+                    Disconsider = disconsider ?? false
                 };
 
                 ctx.Add(file);
-                ctx.SaveChanges();
             }
+            await ctx.SaveChangesAsync();
         }
 
-        public static void AddOrUpdateTableFoldersToVerify(ApplicationDbContext ctx, string? name = null, int? id = null, bool? disconsider = null)
+        public async static Task AddOrUpdateTableFoldersToVerifyAsync(ApplicationDbContext ctx, string? name = null, int? id = null, bool? disconsider = null)
         {
             if (string.IsNullOrWhiteSpace(name) && id == null)
-                throw new Exception("O nome e o ID não podem ser nulos nesse método");
+                throw new ArgumentException("O nome e o ID não podem ser nulos nesse método");
 
-            var fileInDb = ctx.FoldersToVerify
-                .AsEnumerable()
-                .FirstOrDefault(f => (string.IsNullOrWhiteSpace(name) || f.NameFolder.Equals(name, StringComparison.OrdinalIgnoreCase))
-                                     && (id == null || f.Id == id));
+            var fileInDb = await ctx.FoldersToVerify
+                .AsQueryable()
+                .FirstOrDefaultAsync(f => id == null || f.Id == id);
 
             if (fileInDb != null)
             {
-                fileInDb.NameFolder = name;
-                fileInDb.Disconsider = disconsider == null ? false : (bool)disconsider;
-                ctx.SaveChanges();
-
+                fileInDb.NameFolder = string.IsNullOrWhiteSpace(name) ? fileInDb.NameFolder : name;
+                fileInDb.Disconsider = disconsider ?? fileInDb.Disconsider;
             }
             else
             {
                 var folder = new FoldersToVerify
                 {
-                    NameFolder = name,
-                    Disconsider = disconsider == null ? false : (bool)disconsider
+                    NameFolder = string.IsNullOrWhiteSpace(name) ? "Erro!" : name,
+                    Disconsider = disconsider ?? false
                 };
 
                 ctx.Add(folder);
-                ctx.SaveChanges();
             }
+            await ctx.SaveChangesAsync();
         }
 
-        public static void AddOrUpdateTableFilesToVerify(ApplicationDbContext ctx, string? name = null, int? id = null, bool? disconsider = null)
+        public async static Task AddOrUpdateTableFilesToVerifyAsync(ApplicationDbContext ctx, string? name = null, int? id = null, bool? disconsider = null)
         {
             if (string.IsNullOrWhiteSpace(name) && id == null)
-                throw new Exception("O nome e o ID não podem ser nulos nesse método");
+                throw new ArgumentException("O nome e o ID não podem ser nulos nesse método");
 
-            var fileInDb = ctx.FilesToVerify
-                .AsEnumerable()
-                .FirstOrDefault(f => (string.IsNullOrWhiteSpace(name) || f.NameFile.Equals(name, StringComparison.OrdinalIgnoreCase))
-                                     && (id == null || f.Id == id));
+            var fileInDb = await ctx.FilesToVerify
+                .AsQueryable()
+                .FirstOrDefaultAsync(f => id == null || f.Id == id);
 
             if (fileInDb != null)
             {
-                fileInDb.NameFile = name;
-                ctx.SaveChanges();
-
+                fileInDb.NameFile = string.IsNullOrWhiteSpace(name) ? fileInDb.NameFile : name;
+                fileInDb.Disconsider = disconsider ?? fileInDb.Disconsider;
             }
             else
             {
                 var file = new FilesToVerify
                 {
-                    NameFile = name,
-                    Disconsider = disconsider == null ? false : (bool)disconsider
+                    NameFile = string.IsNullOrWhiteSpace(name) ? "Erro!" : name,
+                    Disconsider = disconsider ?? false
                 };
 
                 ctx.Add(file);
-                ctx.SaveChanges();
             }
+            await ctx.SaveChangesAsync();
         }
+
 
         public async static Task DeleteAsync<T>(ApplicationDbContext ctx, string tableName, object id) where T : class
         {
@@ -254,23 +237,19 @@ namespace GeradorDePacotes.Database
                 throw new Exception("Registro não encontrado.");
             }
 
-            // Remove a entidade
             dbSet.Remove(entity);
-
-            // Salva as alterações
             await ctx.SaveChangesAsync();
         }
 
-        public static void AddOrUpdateTableParKeys(ApplicationDbContext ctx, string parName, string parValue)
+        public async static Task AddOrUpdateTableParKeysAsync(ApplicationDbContext ctx, string parName, string parValue)
         {
-            var par = ctx.ParKeys
-                .Where(x => x.ParName.Equals(parName))
-                .FirstOrDefault();
+            var par = await ctx.ParKeys
+                .AsQueryable()
+                .FirstOrDefaultAsync(x => x.ParName.Equals(parName));
 
             if (par != null)
             {
                 par.ParValue = parValue;
-                ctx.SaveChanges();
             }
             else
             {
@@ -281,14 +260,31 @@ namespace GeradorDePacotes.Database
                 };
 
                 ctx.ParKeys.Add(newParKey);
-                ctx.SaveChanges();
             }
+            await ctx.SaveChangesAsync();
         }
 
-        public static string GetParValue(ApplicationDbContext ctx, string parName)
+        public async static Task<string?> GetParValueAsync(ApplicationDbContext ctx, string parName)
         {
-            return ctx.ParKeys.Where(x => x.ParName.Equals(parName)).Select(x => x.ParValue).FirstOrDefault()!;
+            return await ctx.ParKeys
+                .AsQueryable()
+                .Where(x => x.ParName.Equals(parName))
+                .Select(x => x.ParValue)
+                .FirstOrDefaultAsync();
         }
+
+        public async static Task ClearTableAsync(ApplicationDbContext ctx, string tableName)
+        {
+            if (string.IsNullOrWhiteSpace(tableName))
+                throw new ArgumentException("Nome da tabela não pode ser nulo ou vazio.", nameof(tableName));
+
+            var sqlDelete = $"DELETE FROM {tableName}";
+            await ctx.Database.ExecuteSqlRawAsync(sqlDelete);
+
+            var sqlResetSeq = $"DELETE FROM sqlite_sequence WHERE name='{tableName}'";
+            await ctx.Database.ExecuteSqlRawAsync(sqlResetSeq);
+        }
+
 
         #endregion
     }
