c7de245f73521b4fc7a77122b0a333d564f3d7fd
André Ruan <andreruan80@gmail.com>
2024-09-21 00:08:11 -0300

    Feat: Melhoria na geração do zip com uso de Parallel e buffer. Implementado paralisação da ativade assincrona.

--- GeradorDePacotes/Classes/Zip.cs
+++ GeradorDePacotes/Classes/Zip.cs
@@ -1,74 +1,70 @@
-﻿using ReaLTaiizor.Controls;
-using ReaLTaiizor.Util;
-using System.IO.Compression;
+﻿using System.IO.Compression;
+using ReaLTaiizor.Controls;
 
-
-namespace ExcluirArquivosPublicacao.Entities
+namespace GeradorDePacotes.Classes
 {
     public static class Zip
     {
-        public async static Task CreateZipFileAsync(string rootFolder, string pathZipFile, ParrotCircleProgressBar progressBar)
+        public async static Task CreateZipFileAsync(string rootFolder, string pathZipFile, ParrotCircleProgressBar progressBar, CancellationToken token)
         {
-            await Task.Run(() =>
-             {
-                 try
-                 {
-                     using (FileStream zipToCreate = new FileStream(pathZipFile, FileMode.Create))
-                     {
-                         using (ZipArchive archive = new ZipArchive(zipToCreate, ZipArchiveMode.Create))
-                         {
-                             string[] allFiles = Directory.GetFiles(rootFolder, "*.*", SearchOption.AllDirectories);
-                             int totalFiles = allFiles.Length;
-                             int currentFile = 0;
-                             int refresher = 0;
+            
+                string[] allFiles = Directory.GetFiles(rootFolder, "*.*", SearchOption.AllDirectories);
+                int totalFiles = allFiles.Length;
+                int currentFile = 0;
+                object lockObject = new object(); 
 
-                             // Adiciona os arquivos ao arquivo ZIP
-                             foreach (string file in allFiles)
-                             {
-                                 if (file != pathZipFile)
-                                 {
-                                     string entryName = file.Replace(rootFolder, "").TrimStart(Path.DirectorySeparatorChar);
-                                     archive.CreateEntryFromFile(file, entryName);
+                using (FileStream zipToCreate = new FileStream(pathZipFile, FileMode.Create))
+                using (ZipArchive archive = new ZipArchive(zipToCreate, ZipArchiveMode.Create))
+                {
+                    byte[] buffer = new byte[81920]; 
 
-                                     currentFile++;
-                                     var percentage = (((double)currentFile / (double)totalFiles) * 100.00);
+                    await Task.Run(() =>
+                    {
+                        Parallel.ForEach(allFiles, file =>
+                        {
 
-                                     progressBar.Invoke(() =>
-                                     {
-                                         progressBar.percentage = (int)percentage;
-                                         refresher++;
-                                         if (refresher >= 100)
-                                         {
-                                             progressBar.Refresh();
-                                             refresher = 0;
-                                         }
-                                     });
-                                 }
-                             }
-                             // Para finalizar com 100%, pode ser que pare em 99%
-                             progressBar.Invoke(() =>
-                             {
-                                 if (progressBar.percentage < 100)
-                                 {
-                                     progressBar.percentage = 100;
-                                     progressBar.Refresh();
-                                 }
-                             });
-                         }
-                     }
+                            token.ThrowIfCancellationRequested();
+                            if (file != pathZipFile)
+                            {
+                                string entryName = file.Replace(rootFolder, "").TrimStart(Path.DirectorySeparatorChar);
+
+                                lock (lockObject)
+                                {
+                                    ZipArchiveEntry entry = archive.CreateEntry(entryName, CompressionLevel.Fastest);
+                                    using (var entryStream = entry.Open())
+                                    using (var fileStream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read, buffer.Length, true))
+                                    {
+                                        fileStream.CopyTo(entryStream);
+                                    }
+                                }
+
+                                lock (lockObject)
+                                {
+                                    currentFile++;
+                                    var percentage = (double)currentFile / totalFiles * 100.0;
+
+                                    if (currentFile % 100 == 0 || currentFile == totalFiles)
+                                    {
+                                        progressBar.Invoke(() =>
+                                        {
+                                            progressBar.percentage = (int)percentage;
+                                            progressBar.Refresh();
+                                        });
+                                    }
+                                }
+                            }
+                        });
+                    });
+
+                    progressBar.Invoke(() =>
+                    {
+                        progressBar.percentage = 100;
+                        progressBar.Refresh();
+                    });
+                }
 
 
-                     //string[] pastaAdicionadas = Directory.GetDirectories(rootFolder);
-                     //string[] arquivosAdicionados = Directory.GetFiles(rootFolder);
-                     //int totalArquivosAdicionados = pastaAdicionadas.Length + arquivosAdicionados.Length - 1;
-                 }
-                 catch (Exception ex)
-                 {
-                     string message = $"Erro ao criar arquivo ZIP: {ex.Message}";
-                     throw new Exception(message, ex);
-                 }
-             });
+            }
+            
         }
     }
-
-}
