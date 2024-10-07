26896d77b5817ce7b647cdcacbcaf86606f4438e
André Ruan <andreruan80@gmail.com>
2024-10-06 23:00:39 -0300

    Feat: Adicionado prop para quantidade itens adicionados no zip para o relatório final

--- GeradorDePacotes/Classes/Zip.cs
+++ GeradorDePacotes/Classes/Zip.cs
@@ -5,66 +5,66 @@ namespace GeradorDePacotes.Classes
 {
     public static class Zip
     {
+        public static int AddedItemsCount { get; private set; }
         public async static Task CreateZipFileAsync(string rootFolder, string pathZipFile, ParrotCircleProgressBar progressBar, CancellationToken token)
         {
-            
-                string[] allFiles = Directory.GetFiles(rootFolder, "*.*", SearchOption.AllDirectories);
-                int totalFiles = allFiles.Length;
-                int currentFile = 0;
-                object lockObject = new object(); 
+            string[] allFiles = Directory.GetFiles(rootFolder, "*.*", SearchOption.AllDirectories);
+            int totalFiles = allFiles.Length;
+            int currentFile = 0;
+            object lockObject = new object();
 
-                using (FileStream zipToCreate = new FileStream(pathZipFile, FileMode.Create))
-                using (ZipArchive archive = new ZipArchive(zipToCreate, ZipArchiveMode.Create))
+            using (FileStream zipToCreate = new FileStream(pathZipFile, FileMode.Create))
+            using (ZipArchive archive = new ZipArchive(zipToCreate, ZipArchiveMode.Create))
+            {
+                byte[] buffer = new byte[81920];
+
+                await Task.Run(() =>
                 {
-                    byte[] buffer = new byte[81920]; 
-
-                    await Task.Run(() =>
+                    Parallel.ForEach(allFiles, file =>
                     {
-                        Parallel.ForEach(allFiles, file =>
+                        token.ThrowIfCancellationRequested();
+                        if (file != pathZipFile)
                         {
+                            string entryName = file.Replace(rootFolder, "").TrimStart(Path.DirectorySeparatorChar);
 
-                            token.ThrowIfCancellationRequested();
-                            if (file != pathZipFile)
+                            lock (lockObject)
                             {
-                                string entryName = file.Replace(rootFolder, "").TrimStart(Path.DirectorySeparatorChar);
-
-                                lock (lockObject)
+                                ZipArchiveEntry entry = archive.CreateEntry(entryName, CompressionLevel.Fastest);
+                                using (var entryStream = entry.Open())
+                                using (var fileStream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read, buffer.Length, true))
                                 {
-                                    ZipArchiveEntry entry = archive.CreateEntry(entryName, CompressionLevel.Fastest);
-                                    using (var entryStream = entry.Open())
-                                    using (var fileStream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read, buffer.Length, true))
-                                    {
-                                        fileStream.CopyTo(entryStream);
-                                    }
-                                }
-
-                                lock (lockObject)
-                                {
-                                    currentFile++;
-                                    var percentage = (double)currentFile / totalFiles * 100.0;
-
-                                    if (currentFile % 100 == 0 || currentFile == totalFiles)
-                                    {
-                                        progressBar.Invoke(() =>
-                                        {
-                                            progressBar.percentage = (int)percentage;
-                                            progressBar.Refresh();
-                                        });
-                                    }
+                                    fileStream.CopyTo(entryStream);
                                 }
                             }
-                        });
+
+                            lock (lockObject)
+                            {
+                                currentFile++;
+                                var percentage = (double)currentFile / totalFiles * 100.0;
+
+                                if (currentFile % 100 == 0 || currentFile == totalFiles)
+                                {
+                                    progressBar.Invoke(() =>
+                                    {
+                                        progressBar.percentage = (int)percentage;
+                                        progressBar.Refresh();
+                                    });
+                                }
+                            }
+                        }
                     });
+                });
 
-                    progressBar.Invoke(() =>
-                    {
-                        progressBar.percentage = 100;
-                        progressBar.Refresh();
-                    });
-                }
-
-
+                progressBar.Invoke(() =>
+                {
+                    progressBar.percentage = 100;
+                    progressBar.Refresh();
+                });
             }
-            
+
+            // Para o relatório
+            AddedItemsCount = Directory.GetFiles(rootFolder).Length + Directory.GetDirectories(rootFolder).Length;
         }
+
     }
+}
